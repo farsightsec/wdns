43005d6d5eb52b8c571e2ba268d3aff9cc68dffc
Henry Stern <stern@fsi.io>
2014-01-28 23:42:27 -0400

    wdns_reverse_name: fixed buffer overflow on invalid input

--- wdns/reverse_name.c
+++ wdns/reverse_name.c
@@ -1,7 +1,8 @@
-void
+wdns_res
 wdns_reverse_name(const uint8_t *name, size_t len_name, uint8_t *rev_name) {
 	const uint8_t *p;
-	unsigned len;
+	size_t len;
+	size_t total_len = 0;
 
 	p = name;
 	memset(rev_name, 0, len_name);
@@ -9,8 +10,14 @@ wdns_reverse_name(const uint8_t *name, size_t len_name, uint8_t *rev_name) {
 
 	while ((len = *p) != '\x00') {
 		len += 1;
+		total_len += len;
+		if (total_len > len_name) {
+			return (wdns_res_out_of_bounds);
+		}
 		rev_name -= len;
 		memcpy(rev_name, p, len);
 		p += len;
 	}
+
+	return (wdns_res_success);
 }
