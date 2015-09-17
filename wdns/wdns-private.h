266dd2a4848b8fb1733f32ea737027a5ce9d97ea
Henry Stern <stern@fsi.io>
2015-09-17 15:00:20 -0300

    b{32,64}_{en,de}code: complete move to libmy

--- wdns/wdns-private.h
+++ wdns/wdns-private.h
@@ -19,10 +19,10 @@
 #include "wdns.h"
 
 #include "record_descr.h"
-#include "b32_decode.h"
-#include "b32_encode.h"
-#include "b64_decode.h"
-#include "b64_encode.h"
+#include "libmy/b32_decode.h"
+#include "libmy/b32_encode.h"
+#include "libmy/b64_decode.h"
+#include "libmy/b64_encode.h"
 
 #include "libmy/my_alloc.h"
 #include "libmy/ubuf.h"
