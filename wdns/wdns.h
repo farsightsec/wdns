ea66dcea30cf85638f974fd0f586ff32dcc66f64
Henry Stern <stern@fsi.io>
2015-09-21 10:29:27 -0300

    Whitespace corrections

--- wdns/wdns.h
+++ wdns/wdns.h
@@ -251,7 +251,7 @@ wdns_res
 wdns_str_to_name(const char *str, wdns_name_t *name);
 
 wdns_res
-wdns_str_to_rcode(const char *str, uint16_t * out);
+wdns_str_to_rcode(const char *str, uint16_t *out);
 
 uint16_t
 wdns_str_to_rrtype(const char *str);
