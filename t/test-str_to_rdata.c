3f7f21fc5845e3614c8f902625a222a55260f836
Chris Mikkelson <cmikk@fsi.io>
2021-05-04 10:58:26 -0500

    Extend round trip str_to_rdata test logic
    If the rdata formats as a different string from the original input,
    verify that both strings encode the same rdata.

--- t/test-str_to_rdata.c
+++ t/test-str_to_rdata.c
@@ -492,17 +492,50 @@ test_str_to_rdata(void) {
 
 				} else if (strncasecmp(cur->input, roundtrip,
 				    strlen(cur->input)) != 0) {
-					ubuf_add_fmt(u, "\nFAIL %" PRIu64
-					    ": round trip mismatch, "
-					    "rrtype=%s input=",
-					    cur - tdata,
-					    wdns_rrtype_to_str(cur->rrtype),
-					    cur->input);
-					escape(u, (const uint8_t *)cur->input,
-					    strlen(cur->input));
+					/*
+					 * The round trip string differs from the
+					 * original input. Verify that both strings
+					 * encode the same rdata.
+					 */
+					free(actual);
+					actual = NULL;
 
-					ubuf_add_fmt(u, " value=%s", roundtrip);
-					failures++;
+					res = wdns_str_to_rdata(roundtrip, cur->rrtype, cur->rrclass, &actual, &actual_len);
+					if (res != wdns_res_success) {
+						ubuf_add_fmt(u, "\nFAIL %" PRIu64
+							": round trip res=%s,"
+							"rrtype=%s input=",
+							cur - tdata,
+							wdns_res_to_str(res),
+							wdns_rrtype_to_str(cur->rrtype));
+						escape(u, (const uint8_t *)roundtrip,
+							strlen(roundtrip));
+						failures++;
+					} else if ((actual_len != cur->expected_len) ||
+						   memcmp(actual, cur->expected, actual_len)) {
+						ubuf_add_fmt(u, "\nFAIL %" PRIu64
+							": round trip mismatch, "
+							"rrtype=%s input=",
+							cur - tdata,
+							wdns_rrtype_to_str(cur->rrtype));
+						escape(u, (const uint8_t *)roundtrip,
+							strlen(roundtrip));
+						ubuf_add_cstr(u, " value=");
+						escape(u, actual, actual_len);
+						ubuf_add_cstr(u, " != ");
+						escape(u, cur->expected, cur->expected_len);
+						failures++;
+					} else {
+						ubuf_add_fmt(u, "\nPASS %" PRIu64
+							": round trip match, "
+							"rrtype=%s roundtrip=",
+							cur - tdata,
+							wdns_rrtype_to_str(cur->rrtype));
+						escape(u, (const uint8_t *)roundtrip,
+							strlen(roundtrip));
+						ubuf_add_cstr(u, " value=");
+						escape(u, actual, actual_len);
+					}
 
 					free(roundtrip);
 				} else {
