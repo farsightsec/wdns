598cb3fdf929f261dfd9907f43e7157d354b0335
Chris Mikkelson <cmikkelson@domaintools.com>
2023-05-12 15:26:18 +0000

    Fix off-by-one error in wdns_str_to_name
    When parsing a name with a trailing '.', finish parsing at the '.'
    without additionally processing end of input. Fixes a write beyond
    the end of the resulting name, which can be unallocated memory for
    names of maximum length.
    
    Add test cases for maximum length names and name overflow.

--- t/test-str_to_name.c
+++ t/test-str_to_name.c
@@ -40,7 +40,23 @@ struct test {
 
 struct test tdata[] = {
 	{ "fsi.io", (fp)wdns_str_to_name, (const uint8_t*)"\x03""fsi\x02io\x00", 8, wdns_res_success},
+	{ "fsi.io.", (fp)wdns_str_to_name, (const uint8_t*)"\x03""fsi\x02io\x00", 8, wdns_res_success},
 	{ "FsI.io", (fp)wdns_str_to_name_case, (const uint8_t*)"\x03""FsI\x02io\x00", 8, wdns_res_success},
+	{ "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x61xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+	  (fp)wdns_str_to_name,
+	  (const uint8_t *)"\x3Fx63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+			   "\x3Fx63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+			   "\x3Fx63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+			   "\x3Dx61xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\x00",
+	  255, wdns_res_success },
+	{ "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x63xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx."
+	  "x62xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+	  (fp)wdns_str_to_name, (const uint8_t *)"", 0, wdns_res_name_overflow },
 	{ 0 }
 };
 
@@ -74,6 +90,10 @@ test_str_to_name(void)
 				escape(u, name.data, name.len);
 			}
 			failures++;
+		} else if (res != wdns_res_success) {
+			ubuf_add_fmt(u, "PASS %" PRIu64 ": input=", cur-tdata);
+			escape(u, (uint8_t*)cur->input, strlen(cur->input));
+			ubuf_add_fmt(u, " res=%s", wdns_res_to_str(res));
 		} else if (name.len != cur->expected_len || memcmp(name.data, cur->expected, name.len)) {
 			ubuf_add_fmt(u, "FAIL %" PRIu64 ": input=", cur-tdata);
 			escape(u, (uint8_t*)cur->input, strlen(cur->input));
