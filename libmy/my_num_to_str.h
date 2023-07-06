7cbeaf4d6e38edfab9e4b7c52bc9aa9936f7dcf0
Demian Vladi <dvladi@domaintools.com>
2023-07-06 08:38:06 -0700

    Changed header file

--- libmy/my_num_to_str.h
+++ libmy/my_num_to_str.h
@@ -5,34 +5,26 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-/**
- * Formats bytes in src to hexadecimal null terminated, front padded with 0 string:
- * 	0x1f1e1d -> '1f1e1d\0'
- * 	0xf -> '0f\0'
- *
- * \return Pointer to dst.
+/*
+ * Format bytes as a null-terminated hex string, with each byte represented as two characters.
+ * Returns a pointer to the newly formatted string.
  */
-const char * my_bytes_to_hex_str(const uint8_t *src, size_t len, bool is_upper, char *dst);
+const char *my_bytes_to_hex_str(const uint8_t *src, size_t len, bool is_upper, char *dst);
 
-/**
- * Formats num to hexadecimal string.
- * does not terminate with 0
- *
- * \return Pointer to place in dst after inserted symbols.
+/*
+ * Format num as hex string, but without a nul terminating byte.
+ * Returns a pointer to the newly formatted string.
  */
-const char * my_uint16_to_hex_str(uint16_t num, bool is_upper, char *dst);
+const char *my_uint16_to_hex_str(uint16_t num, bool is_upper, char *dst);
 
-/**
- * Formats num as numerical string front padded with 0.
- * does not terminate with 0
- * \return Pointer to dst.
+/*
+ * Format num as numerical string, front padded with zeroes to size chars, without a nul terminating byte.
+ * Returns a pointer to the newly formatted string.
  */
 const char *my_uint64_to_str_padded(uint64_t num, int size, char *dst);
 
-/**
- * Formats num as numerical string 0 terminated string.
- *
- * \return Size of numerical string.
+/*
+ * Format num as null-terminated numerical string, returning the string length.
  */
 size_t my_uint64_to_str(uint64_t num, char *dst);
 
